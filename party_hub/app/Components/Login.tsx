fb15bfd8774d5b62d712ead4df7f3891c34e6977
Nitesh Tagadghar <niteshtagadghar3@gmail.com>
2023-10-30 16:19:12 +0530

    created a login page

---
+++ party_hub/app/Components/Login.tsx
@@ -0,0 +1,95 @@
+"use client"
+import React, {useState} from 'react';
+
+function Login() {
+    const [idDetail, setIdDetails] = useState(
+        {
+          emailId: "",
+          password: ""
+        }
+      )
+    
+      const submitSignIn = ()=>{
+        console.log(idDetail,'id detail in on submit')
+       localStorage.setItem(idDetail.emailId,'email_id')
+       localStorage.setItem(idDetail.password,'password')
+        
+        
+      }
+    
+
+
+  return (
+    <div className="bg-gray-50 dark:bg-gray-900">
+    <div className="flex flex-col items-center justify-center min-h-screen px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div className="flex">
+        <a href="#" className="flex items-center mb-6 text-4xl md:text-6xl font-bold text-orange-500 dark:text-white">
+          Party Hub
+        </a>
+        <img src="https://cdn-icons-png.flaticon.com/128/12068/12068404.png" className="ml-4 h-14 w-14" alt="Party Hub Logo" />
+      </div>
+      <div className="w-full bg-white rounded-lg shadow md:border dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+
+          <h1 className="text-xl md:text-2xl font-semibold leading-tight tracking-tight text-gray-900 dark:text-white">
+            Sign in to your account
+          </h1>
+          <form className="space-y-4 md:space-y-6" action="#" onSubmit={submitSignIn}>
+            <div>
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="name@company.com"
+                required
+                onChange={(e) => setIdDetails({ ...setIdDetails, emailId: e.target.value ,password:idDetail.password})}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="••••••••"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+                onChange={(e)=>setIdDetails({...setIdDetails,  password:e.target.value , emailId:idDetail.emailId})}
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <div className="flex items-start">
+                <div className="flex items-center h-5">
+                  <input
+                    id="remember"
+                    aria-describedby="remember"
+                    type="checkbox"
+                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                  />
+                </div>
+                <div className="ml-3 text-sm">
+                  <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                </div>
+              </div>
+              <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+            </div>
+            <button
+              type="submit"
+              className="w-full text-white bg-orange-600 hover:bg-orange-700 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Sign in
+            </button>
+            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+              Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Login
